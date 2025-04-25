5197b494a7cf1b697108437a498798de9686e89d
mxinseo <kyungminseo10@gmail.com>
2025-04-25 15:39:11 +0900

    Feat : 중복name, 불필요한 지점 제거, 포맷팅

---
+++ 한국등산트레킹지원센터_100대명산/python/0_json_cleaning.py
@@ -0,0 +1,47 @@
+import os
+import json
+
+# 경로 설정
+base_dir = os.path.dirname(__file__)
+json_path = os.path.join(base_dir, '../mnt_100_convert_json')
+convert_path = os.path.join(base_dir, '../mnt_100_convert_json_clean')
+os.makedirs(convert_path, exist_ok=True)
+
+# 제거할 요소 리스트
+elements_to_remove = [
+    "낙석", "의약품 함", "낙뢰 및 우천시 행동 요령", "재해위험지구", 
+    "추락주의", "산불감시초소", "낙석 위험", "낙석 위험지역", "쉼터", "사고위험지구",
+    "조망점", "주차장"
+]
+
+# 불필요한 요소를 제거하는 함수
+def remove_element(json_data):
+    if isinstance(json_data, list):
+        json_data = [item for item in json_data if item["name"] not in elements_to_remove]
+    return json_data
+
+def process_json_file(input_file):
+    with open(input_file, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        
+    # 불필요한 요소 제거
+    cleaned_data = remove_element(data)
+    
+    # 변환된 데이터 저장 경로 설정
+    relative_path = os.path.relpath(input_file, json_path)  # 상대 경로 계산
+    output_file = os.path.join(convert_path, relative_path)
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)  # 폴더 생성
+    
+    with open(output_file, 'w', encoding='utf-8') as file:
+        json.dump(cleaned_data, file, ensure_ascii=False, indent=4)
+
+# 모든 폴더 내의 파일 처리
+def process_all_json_files():
+    for root, dirs, files in os.walk(json_path):
+        for filename in files:
+            if filename.endswith('.json'):
+                input_file = os.path.join(root, filename)
+                process_json_file(input_file)
+
+# 실행
+process_all_json_files()
